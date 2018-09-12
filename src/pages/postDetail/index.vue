97879a0c876c29ede6608669e855a6e9a9e675d2
Ti-tanium <1158976264@qq.com>
2018-09-12 10:24:28 +0800

    add post detail page

---
+++ src/pages/postDetail/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="postDetail-container">
+   post-detail {{itemId}}
+  </div>
+</template>
+
+<script>
+import { get } from '@/utils/index'
+import config from '@/config'
+export default {
+  data () {
+    return {
+      itemId: ''
+    }
+  },
+  mounted () {
+    this.itemId = this.$root.$mp.query.id
+    this.getPostDetail()
+  },
+  methods: {
+    async getPostDetail () {
+      const postDetail = await get(config.host + '/weapp/postdetail', {
+        itemId: this.itemId
+      })
+      console.log(postDetail)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
