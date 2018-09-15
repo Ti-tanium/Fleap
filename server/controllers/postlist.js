4bacfd7b92ea8a4975857f8f72d5885ccf95bc88
Ti-tanium <1158976264@qq.com>
2018-09-15 15:25:34 +0800

    postlist 接口增加分类

--- server/controllers/postlist.js
+++ server/controllers/postlist.js
@@ -2,11 +2,16 @@ const { mysql } = require('../qcloud')
 
 module.exports = async ctx => {
     const { category, Free } = ctx.request.query
-    console.log(category, Free)
+    console.log('category:\n', category, Free)
     const sql = mysql('merchandise')
-    let postList = await sql.where('category', category)
+    let postList
+    let price = 0.0
     if (Free) {
-        postList = await sql.where('price', 0)
+        postList = await sql.where('price', price)
+    } else if (category === 'all') {
+        postList = await sql.select('merchandise.*')
+    } else {
+        postList = await sql.where('category', category)
     }
     ctx.state.data = {
         list: postList
--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -4,6 +4,6 @@ module.exports = async ctx => {
     // 获取上传之后的结果
     // 具体可以查看：
     const data = await uploader(ctx.req)
-
+    console.log(data)
     ctx.state.data = data
 }
