d3e89747dcd851f61cc28b7cf2953bdfbb1d610f
Ti-tanium <1158976264@qq.com>
2018-09-19 10:46:42 +0800

    online testing configuration

--- server/config.js
+++ server/config.js
@@ -2,15 +2,22 @@ const CONF = {
     port: '5757',
     rootPathname: '',
 
-    serverHost: 'localhost',
-    tunnelServerUrl: '',
-    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
-    // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
-    qcloudAppId: '1257251040',
-    qcloudSecretId: 'AKIDeEwbzkJ1f8lK0cCPgyBIPQHzWzaQmtli',
-    qcloudSecretKey: 'YDkEhFpJhz7AylQ77HmLn9bwLPQJ22wK',
-    wxMessageToken: 'weixinmsgtoken',
-    networkTimeout: 30000,
+    // serverHost: '',
+    // tunnelServerUrl: '',
+    // tunnelSignatureKey: '',
+    // qcloudAppId: '',
+    // qcloudSecretId: '',
+    // qcloudSecretKey: '',
+
+    // serverHost: 'localhost',
+    // tunnelServerUrl: '',
+    // tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+    // // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
+    // qcloudAppId: '1257251040',
+    // qcloudSecretId: 'AKIDeEwbzkJ1f8lK0cCPgyBIPQHzWzaQmtli',
+    // qcloudSecretKey: 'YDkEhFpJhz7AylQ77HmLn9bwLPQJ22wK',
+    // wxMessageToken: 'weixinmsgtoken',
+    // networkTimeout: 30000,
 
     // 微信小程序 App ID
     appId: 'wx77ceeae018c3f9bc',
@@ -31,8 +38,8 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        // pass: 'wx77ceeae018c3f9bc',
-        pass: '1158976264',
+        pass: 'wx77ceeae018c3f9bc',
+        // pass: '1158976264',
         char: 'utf8mb4'
     },
 
--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
-const host = 'http://localhost:5757'
+// const host = 'http://localhost:5757'
+
+const host = 'https://iuivr6vb.qcloud.la'
 
 const config = {
   host,
-  loginUrl: host + '/weapp/login'
+  loginUrl: host + '/weapp/login',
+  uploadUrl: host + '/weapp/upload'
 }
 
 export default config
