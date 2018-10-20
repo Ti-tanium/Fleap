36a7cfeacedec43b4e8825a2575bac2eba3465cb
Ti-tanium <1158976264@qq.com>
2018-10-20 17:59:12 +0800

    add showloading to my post page

--- src/pages/myPost/index.vue
+++ src/pages/myPost/index.vue
@@ -26,12 +26,18 @@ export default {
     async getPostList () {
       const userinfo = wx.getStorageSync('userinfo')
       const openId = userinfo.openId
+      wx.showLoading({
+        title: '加载中...', // 提示的内容,
+        mask: true, // 显示透明蒙层，防止触摸穿透,
+        success: res => {}
+      })
       const postList = await get(config.host + '/weapp/postlist', {
         category: 'all',
         openId: openId
       })
       console.log(postList.data.list)
       this.postList = postList.data.list
+      wx.hideLoading()
     }
   }
 }
