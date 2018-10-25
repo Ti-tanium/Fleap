b6c60002703bd6c80356caec45c3de1b977ab3bc
Ti-tanium <1158976264@qq.com>
2018-10-25 17:30:56 +0800

    增加腾讯云字段
    由于API的改动，不增加这三个字段，上传图片时会出现authorization error

--- server/config.js
+++ server/config.js
@@ -13,9 +13,9 @@ const CONF = {
     // tunnelServerUrl: '',
     // tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
     // // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
-    // qcloudAppId: '1257251040',
-    // qcloudSecretId: 'AKIDeEwbzkJ1f8lK0cCPgyBIPQHzWzaQmtli',
-    // qcloudSecretKey: 'YDkEhFpJhz7AylQ77HmLn9bwLPQJ22wK',
+    qcloudAppId: '1257251040',
+    qcloudSecretId: 'AKIDeEwbzkJ1f8lK0cCPgyBIPQHzWzaQmtli',
+    qcloudSecretKey: 'YDkEhFpJhz7AylQ77HmLn9bwLPQJ22wK',
     // wxMessageToken: 'weixinmsgtoken',
     // networkTimeout: 30000,
 
