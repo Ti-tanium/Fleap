747f25cba20c4bafaef95ae55a620be0ef9eb677
Ti-tanium <1158976264@qq.com>
2019-03-04 12:31:26 +0800

    Update postdetail.js
    using new database

--- server/controllers/postdetail.js
+++ server/controllers/postdetail.js
@@ -3,13 +3,33 @@ const { mysql } = require('../qcloud')
 module.exports = async ctx => {
     const { itemId } = ctx.request.query
     console.log('itemId:', itemId)
-    const postDetail = await mysql('merchandise')
+    try {
+        const postDetail = await mysql('post')
         .select('*')
         .where('id', itemId)
+        const userinfo = await mysql('user').select('QQId', 'phone').where('openId', postDetail[0].openId)
+        var detail = {}
+        Object.assign(detail, userinfo[0], postDetail[0])
+        ctx.state.data = detail
+    } catch (e) {
+        console.log(e)
+        ctx.state = {
+            code: -1,
+            msg: 'Get post Detail error:' + e.sqlMessage
+        }
+        console.log('Get post Detail error:' + e.sqlMessage)
+    }
 
-    ctx.state.data = postDetail[0]
-    // view count
-    await mysql('merchandise')
+    try {
+        // view count
+        await mysql('post')
         .where('id', itemId)
         .increment('viewCount', 1)
+    } catch (e) {
+        ctx.state = {
+            code: -1,
+            msg: 'increase view count error:' + e.sqlMessage
+        }
+        console.log('increase view count error:' + e.sqlMessage)
+    }
 }
