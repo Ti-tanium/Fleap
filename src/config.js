a2fe7f90569ce30a0dfba5d2d88810068b4acee1
Ti-tanium <1158976264@qq.com>
2019-03-26 16:24:19 +0800

    add route url

--- src/config.js
+++ src/config.js
@@ -1,11 +1,18 @@
 // const host = 'http://localhost:5757'
-
 const host = 'https://iuivr6vb.qcloud.la'
 
 const config = {
   host,
   loginUrl: host + '/weapp/login',
-  uploadUrl: host + '/weapp/upload'
+  uploadUrl: host + '/weapp/upload',
+  saveuserinfoUrl: host + '/weapp/saveuserinfo',
+  getAddUserInfoUrl: host + '/weapp/getadduserinfo',
+  getPostsUrl: host + '/weapp/getposts',
+  getMyPostsUrl: host + '/weapp/getmyposts',
+  soldUrl: host + '/weapp/sold',
+  collectUrl: host + '/weapp/collect',
+  getMyCollectionUrl: host + '/weapp/getmycollection',
+  searchUrl: host + '/weapp/search'
 }
 
 export default config
